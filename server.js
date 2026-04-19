8a8a77511e7d5c7fcea9d5089fe9c0214c112642
Mohamed Elsayed <mo.elsayed@aucegypt.edu>
2026-04-19 23:24:53 +0200

    final test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const mysql = require("mysql2");
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2";
 
 const app = express();
 app.use(cors({
